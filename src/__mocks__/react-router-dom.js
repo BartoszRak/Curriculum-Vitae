e7ac6c5a013546c63072605fc48f7fff524f7a05
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-04-17 00:22:14 +0200

    feat: Tests configuraiton.

---
+++ src/__mocks__/react-router-dom.js
@@ -0,0 +1,9 @@
+import { makeGetProxy } from '../utils/proxies'
+
+// Custom mocks
+const definedMocks = {
+  withRouter: jest.fn(() => 'withRouterMock'),
+}
+
+// Mock any not defined components automatically: Component -> ComponentMock
+module.exports = makeGetProxy(definedMocks, (target, name) => `${name}Mock`)
