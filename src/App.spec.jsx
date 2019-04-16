c67eedd6867ec080eb70958070c345a3e6b95000
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-04-17 00:25:32 +0200

    feat: Test configuration test, App.jsx test file

---
+++ src/App.spec.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { App } from './App'
+
+const initComponent = overrides => {
+  const mockProps = {
+    classes: {},
+  }
+  const mockMethods = {}
+  const wrapper = shallow(<App {...mockProps} {...mockMethods} {...overrides} />)
+  return { mockProps, wrapper }
+}
+
+describe('global: App', () => {
+  it('renders without crashing', () => {
+    const { wrapper } = initComponent()
+    expect(wrapper).toBeTruthy()
+  })
+})
