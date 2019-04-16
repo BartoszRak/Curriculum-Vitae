f5fd92b988513994576ddd510da47cb2c7eb77db
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-04-17 01:01:44 +0200

    feat: Homepage component added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom'
+
 import withMuiTheme from './theme/withMuiTheme'
+import Home from './pages/Home'
 
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        app
-      </div>
+      <BrowserRouter>
+      <Switch>
+        <Route component={Home} to="/" />
+        <Redirect from="*" to="/" />
+      </Switch>
+      </BrowserRouter>
     )
   }
 }
