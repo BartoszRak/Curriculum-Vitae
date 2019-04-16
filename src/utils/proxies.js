317badbc950f42182026dd8f1b60c34257f83ff1
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-04-17 00:22:14 +0200

    feat: Tests configuraiton.

---
+++ src/utils/proxies.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export const makeGetProxy = (obj, missingKeyHandler) =>
+  new Proxy(obj, {
+    get(target, name) {
+      return name in target ? target[name] : missingKeyHandler(target, name)
+    },
+  })
+
+export default makeGetProxy
