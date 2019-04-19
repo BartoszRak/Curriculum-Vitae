2e0240643593c4707ae7bd5f205bafeb61466929
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-04-20 01:16:49 +0200

    feature: Eslint fixed, footer text added.

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
+import { withStyles, Typography } from '@material-ui/core/'
 import PropTypes from 'prop-types'
 
 import style from './Footer.style'
 
 export const Footer = ({ classes }) => (
   <div className={classes.root}>
-    Footer
+    <Typography className={classes.text} variant="body2">
+      &copy; All copyrights reserved 2019-
+      {new Date().getFullYear()}
+    </Typography>
   </div>
 )
 
