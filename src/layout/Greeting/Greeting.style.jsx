3abee06879ca2e71c9c486c79c13cf3c6b5c4521
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-04-23 13:20:55 +0200

    feat: New darker background.

--- src/layout/Greeting/Greeting.style.jsx
+++ src/layout/Greeting/Greeting.style.jsx
@@ -1,7 +1,9 @@
 export default () => ({
   root: {
     alignItems: 'center',
+    backgroundAttachment: 'fixed',
     display: 'flex',
+    flexDirection: 'column',
     height: '100vh',
     justifyContent: 'center',
   },
