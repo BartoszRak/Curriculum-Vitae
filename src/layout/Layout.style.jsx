1c9e42ae9aafc23d70b1d423696ae3745957b753
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-05-14 03:22:42 +0200

    feat: Add temporary progress information.

--- src/layout/Layout.style.jsx
+++ src/layout/Layout.style.jsx
@@ -1,17 +1,27 @@
-export default ({ palette }) => ({
+export default ({ palette, spacing }) => ({
   bottomShadow: {
-    background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.10) 50%, rgba(0, 0, 0, 0.30)100%)',
-    width: '100%',
-    height: 96,
-    position: 'fixed',
+    background:
+    'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.10) 50%, rgba(0, 0, 0, 0.30)100%)',
     bottom: 0,
+    height: 96,
     left: 0,
+    position: 'fixed',
+    width: '100%',
+  },
+  information: {
+    background: palette.error.main,
+    color: palette.secondary.main,
+    padding: spacing.unit * 1.5,
+    position: 'fixed',
+    right: 0,
+    top: 0,
+    zIndex: 100000000000,
   },
   root: {
-    justifyContent: 'flex-start',
+    backgroundColor: palette.primary.dark,
     display: 'flex',
     flexDirection: 'column',
+    justifyContent: 'flex-start',
     minHeight: '100vh',
-    backgroundColor: palette.primary.dark,
   },
 })
