88a8c71e2a22787defc4a22a4401482931a578d5
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-05-16 11:05:17 +0200

    feat: Add internacionalization by context.

--- src/components/LanguageSelect/LanguageSelect.jsx
+++ src/components/LanguageSelect/LanguageSelect.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles, Select, MenuItem } from '@material-ui/core'
 
-import { useSelect } from '~hooks'
+import { LanguageContext } from '~services/Internacionalization'
 
 import style from './LanguageSelect.style'
 
 export function LanguageSelect({ classes }) {
-  const { selected, setSelected } = useSelect('en')
   return (
-    <div className={classes.root}>
-      <Select
-        name="Language"
-        onChange={evt => {
-          setSelected(evt.target.value)
-        }}
-        value={selected}
-        variant="contained"
-      >
-        <MenuItem value="en">English</MenuItem>
-        <MenuItem value="pl">Polish</MenuItem>
-      </Select>
-    </div>
+    <LanguageContext.Consumer>
+      {({ language, setLanguage }) => (
+        <div className={classes.root}>
+          <Select
+            name="Language"
+            onChange={evt => {
+              setLanguage(evt.target.value)
+            }}
+            value={language}
+            variant="standard"
+          >
+            <MenuItem value="en">English</MenuItem>
+            <MenuItem value="pl">Polish</MenuItem>
+          </Select>
+        </div>
+      )}
+    </LanguageContext.Consumer>
   )
 }
 
