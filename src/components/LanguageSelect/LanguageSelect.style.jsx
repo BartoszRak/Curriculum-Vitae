ff0497b6ccfb191c23cfeb2b18a5accdaa13e0b1
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-05-16 22:33:49 +0200

    feat: Improve language selection.

--- src/components/LanguageSelect/LanguageSelect.style.jsx
+++ src/components/LanguageSelect/LanguageSelect.style.jsx
@@ -1,6 +1,33 @@
-export default ({ palette }) => ({
+export default ({ palette, spacing }) => ({
+  menuPaper: {
+    '& li': {
+      fontSize: '0.9rem',
+      padding: `${spacing.unit * 0.5}px ${spacing.unit * 3}px`,
+    },
+  },
   root: {},
   select: {
-    color: palette.secondary.main,
+    '&:after': {
+      display: 'none',
+    },
+    '&:before': {
+      display: 'none',
+    },
+  },
+  selectIcon: {
+    color: palette.primary.main,
+    marginRight: spacing.unit * 1,
+  },
+  selectMenu: {
+    '&:focus': {
+      background: 'none',
+    },
+  },
+  selectRoot: {
+    background: palette.secondary.main,
+    borderRadius: 100,
+    color: palette.primary.main,
+    fontSize: '0.9rem',
+    padding: `0 ${spacing.unit * 2}px`,
   },
 })
