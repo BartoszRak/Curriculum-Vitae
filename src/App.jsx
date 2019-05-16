bff17ec965ce7b3f121f06e433971657f6b0db94
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-05-16 11:05:17 +0200

    feat: Add internacionalization by context.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { BrowserRouter } from 'react-router-dom'
 import { withStyles } from '@material-ui/core'
+import { addLocaleData, IntlProvider } from 'react-intl'
+import { flatten } from 'flat'
+import localeEn from 'react-intl/locale-data/en'
+import localePl from 'react-intl/locale-data/pl'
 
 import withMuiTheme from '~theme/withMuiTheme'
 import Layout from './layout'
 import Loader from '~components/Loader'
 import { usePayloadLoading } from '~hooks'
+import { LanguageContext } from '~services/Internacionalization'
+import messagesPl from '~services/Internacionalization/translations/pl.json'
+import messagesEn from '~services/Internacionalization/translations/en.json'
 
 import style from './App.style'
 
+const messages = {
+  en: flatten(messagesEn),
+  pl: flatten(messagesPl),
+}
+addLocaleData([...localeEn, ...localePl])
 export function App({ classes }) {
   const { loading } = usePayloadLoading()
+  const [language, setLanguage] = useState('en')
   return (
     <BrowserRouter>
-      <Loader className={classes.loader} disable={!loading}>
-        <Layout />
-      </Loader>
+      <LanguageContext.Provider
+        value={{
+          language,
+          setLanguage,
+        }}
+      >
+        <LanguageContext.Consumer>
+          {({ language: chosenLanguage }) => (
+            <IntlProvider
+              key={chosenLanguage}
+              locale={chosenLanguage}
+              messages={messages[chosenLanguage]}
+            >
+              <Loader className={classes.loader} disable={!loading}>
+                <Layout />
+              </Loader>
+            </IntlProvider>
+          )}
+        </LanguageContext.Consumer>
+      </LanguageContext.Provider>
     </BrowserRouter>
   )
 }
