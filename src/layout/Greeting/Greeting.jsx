a5a7c4e37aeb82c9635780a743717f0fa65e64a1
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-06-02 01:55:21 +0200

    ref: Update Greeting styles and tests.

--- src/layout/Greeting/Greeting.jsx
+++ src/layout/Greeting/Greeting.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
-import { withStyles, Typography, Avatar } from '@material-ui/core'
+import { Typography, Avatar } from '@material-ui/core'
 import PropTypes from 'prop-types'
-
 
 import avatarUrl from '~assets/images/instagram-photo.jpg'
 
-import style from './Greeting.style'
+import useStyle from './Greeting.style'
 
-export function Greeting({ classes }) {
+export function Greeting({ classes: overridingClasses }) {
+  const classes = { ...useStyle(), ...overridingClasses }
   return (
     <header
       className={classes.root}
     >
       <Avatar alt="Author instagram picture" className={classes.avatar} src={avatarUrl} />
-      <Typography className={classes.title} color="secondary" variant="h1">
+      <Typography className={classes.title} color="secondary" data-testid="greeting-title" variant="h1">
         Bartosz Rak
       </Typography>
-      <Typography className={classes.subtitle} color="secondary" variant="h2">
+      <Typography className={classes.subtitle} color="secondary" data-testid="greeting-subtitle" variant="h2">
         <FormattedMessage id="layout.greeting.subtitle" />
         <span aria-hidden>😁</span>
       </Typography>
-      <Typography className={classes.subsubtitle} color="secondary" variant="h3">
+      <Typography className={classes.subsubtitle} color="secondary" data-testid="greeting-subsubtitle" variant="h3">
         <FormattedMessage id="layout.greeting.subsubtitle" />
       </Typography>
     </header>
@@ -32,4 +32,4 @@ Greeting.propTypes = {
   classes: PropTypes.object,
 }
 
-export default withStyles(style)(Greeting)
+export default Greeting
