1b8191fe7573287cecf288715b9de19aed59d314
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-06-02 00:16:17 +0200

    ref: Update ProjectTile styles and tests.

--- src/pages/Home/Projects/ProjectsList/ProjectTile/ProjectTile.jsx
+++ src/pages/Home/Projects/ProjectsList/ProjectTile/ProjectTile.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import {
-  withStyles,
   Typography,
   Card,
   CardContent,
@@ -14,12 +13,13 @@ import { Favorite } from '@material-ui/icons'
 
 import { useFavoriteProjectsStorage } from '~hooks'
 
-import style from './ProjectTile.style'
+import useStyle from './ProjectTile.style'
 
 export function ProjectTile({
-  classes,
+  classes: overridingClasses,
   project: { description, title, url, tags = [], isFavorite },
 }) {
+  const classes = { ...useStyle(), ...overridingClasses }
   const { toggleProject } = useFavoriteProjectsStorage()
   const [favorite, setFavorite] = useState(isFavorite)
 
@@ -62,6 +62,7 @@ export function ProjectTile({
         </div>
         <IconButton
           className={classNames({ [classes.favoriteIconButton]: favorite })}
+          data-testid="project-tile-icon-button"
           onClick={() => {
             setFavorite(!favorite)
             toggleProject(url)
@@ -90,4 +91,4 @@ ProjectTile.propTypes = {
   }),
 }
 
-export default withStyles(style)(ProjectTile)
+export default ProjectTile
