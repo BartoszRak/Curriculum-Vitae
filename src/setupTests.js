06706535c9eefac28d9d7a8eb3b14719ebf9dc8d
Bartosz Rak <49698017+BartoszRak@users.noreply.github.com>
2019-07-15 11:54:52 +0200

    feat: Upgrade packages.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,7 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import 'jest-enzyme'
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() })
