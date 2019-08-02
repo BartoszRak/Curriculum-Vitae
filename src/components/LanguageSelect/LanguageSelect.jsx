7e3c0e6d74049ae55d2414336f06836f35121f91
Bartosz Rak <bartosz.rak.official@gmail.com>
2019-08-03 00:07:49 +0200

    feat: Use context hook instead of context consumer

--- src/components/LanguageSelect/LanguageSelect.jsx
+++ src/components/LanguageSelect/LanguageSelect.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Select, MenuItem } from '@material-ui/core'
 import { FormattedMessage } from 'react-intl'
 
@@ -8,41 +8,38 @@ import useStyle from './LanguageSelect.style'
 
 export function LanguageSelect() {
   const classes = useStyle()
+  const { language, setLanguage } = useContext(LanguageContext)
   return (
-    <LanguageContext.Consumer>
-      {({ language, setLanguage }) => (
-        <div className={classes.root}>
-          <Select
-            aria-label="Choose language"
-            className={classes.select}
-            classes={{
-              icon: classes.selectIcon,
-              root: classes.selectRoot,
-              selectMenu: classes.selectMenu,
-            }}
-            data-testid="language-select"
-            MenuProps={{
-              classes: {
-                paper: classes.menuPaper,
-              },
-            }}
-            name="Language"
-            onChange={evt => {
-              setLanguage(evt.target.value)
-            }}
-            value={language || 'en'}
-            variant="standard"
-          >
-            <MenuItem id="language-item-en" value="en">
-              <FormattedMessage id="components.languageSelect.languages.en" />
-            </MenuItem>
-            <MenuItem id="language-item-pl" value="pl">
-              <FormattedMessage id="components.languageSelect.languages.pl" />
-            </MenuItem>
-          </Select>
-        </div>
-      )}
-    </LanguageContext.Consumer>
+    <div className={classes.root}>
+      <Select
+        aria-label="Choose language"
+        className={classes.select}
+        classes={{
+          icon: classes.selectIcon,
+          root: classes.selectRoot,
+          selectMenu: classes.selectMenu,
+        }}
+        data-testid="language-select"
+        MenuProps={{
+          classes: {
+            paper: classes.menuPaper,
+          },
+        }}
+        name="Language"
+        onChange={evt => {
+          setLanguage(evt.target.value)
+        }}
+        value={language || 'en'}
+        variant="standard"
+      >
+        <MenuItem id="language-item-en" value="en">
+          <FormattedMessage id="components.languageSelect.languages.en" />
+        </MenuItem>
+        <MenuItem id="language-item-pl" value="pl">
+          <FormattedMessage id="components.languageSelect.languages.pl" />
+        </MenuItem>
+      </Select>
+    </div>
   )
 }
 
